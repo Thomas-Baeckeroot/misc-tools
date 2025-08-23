70631c620255fef0be6a1e683c09bed9deaf1d29
Thomas Baeckeroot <tbaecker.spam@gmail.com>
2025-08-23 12:44:14 +0200

    Conversion to avif (and jpeg for compatibility and PDFs).

--- scanner/convert-all-png2jpg.sh
+++ scanner/convert-all-png2jpg.sh
@@ -41,12 +41,21 @@ for filename in *.png
 do
 	echo Converting file \"$filename\"...
 	[ -e "$filename" ] || continue
-	dest_jpg="${filename/.png/.jpg}"
+
+	file_processed="${filename/.png/_processed.png}"
+	# Tests for values of -contrast-stretch :
+	# 2%x85% to 2%x90% White paper not 100% white and file size bigger than 92%
+	# 2%x92% BEST for text, some white paper not 100% white but best file size
+	# 2%x94% to 2%x96% Only from 96% white paper is fully white but other text are burnt
+	convert -auto-level -contrast-stretch 2%x90% "$filename" "$file_processed"
+
 	dest_web_jpg="${filename/.png/_web.jpg}"
 	# Convert all .png to .jpg
-	convert -auto-level -quality 40 "$filename" "$dest_web_jpg"
+	convert -auto-level -quality 40 "$file_processed" "$dest_web_jpg"
 	# if below trash-put fails, please 'sudo apt install trash-cli'
-	convert -auto-level "$filename" "$dest_jpg" && trash-put "$filename"
-	# If failing below: sudo apt install trash-cli
-done
 
+	dest_avif="${filename/.png/.avif}"
+	# If failing below because of avifenc': sudo apt install libavif-tools
+	# If failing below because of trash-put': sudo apt install trash-cli
+	avifenc --qcolor 30 --jobs all "$file_processed" "$dest_avif" && trash-put "$filename" && trash-put "$file_processed"
+done
